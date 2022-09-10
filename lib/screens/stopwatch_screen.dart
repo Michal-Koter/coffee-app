54c374b0c28b2dfc9178d44de3a669224f19cc70
KoterMichal <101457707+KoterMichal@users.noreply.github.com>
2022-09-10 21:16:08 +0200

    Create stopwatch screen

---
+++ lib/screens/stopwatch_screen.dart
@@ -0,0 +1,85 @@
+import 'package:first_app/shared/menu_bottom.dart';
+import 'package:first_app/shared/menu_drawer.dart';
+import 'package:flutter/material.dart';
+import '../data/stopwatch.dart';
+
+class StopwatchScreen extends StatefulWidget {
+  const StopwatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopwatchScreen> createState() => _StopwatchScreenState();
+}
+
+class _StopwatchScreenState extends State<StopwatchScreen> {
+  bool isRun = false;
+  Stopwatch stopwatch = Stopwatch();
+  String dif = '00:00:00.00';
+  String buttonText = 'Start';
+  TextStyle buttonStyle =  TextStyle(fontSize: 32, backgroundColor: Colors.green, color: Colors.black);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Stopwatch',
+        ),
+      ),
+      drawer: const MenuDrawer(),
+      bottomNavigationBar: const MenuBottom(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            children: [
+              button(context),
+              Text(dif, style: const TextStyle(fontSize: 32),),
+              Text(isRun.toString()),
+              Text(stopwatch.start.toString()),
+              Text(stopwatch.end.toString())
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget button(context) {
+    return TextButton(
+        onPressed: () {
+          if (isRun) {
+            isRun = false;
+            stopwatch.setStop();
+          } else {
+            isRun = true;
+            stopwatch.setStart();
+            show();
+          }
+          setState(() {
+            if (isRun) {
+              buttonText = 'Stop';
+              buttonStyle =  TextStyle(fontSize: 32, backgroundColor: Colors.red, color: Colors.black);
+            } else {
+              buttonText = 'Start';
+              buttonStyle =  TextStyle(fontSize: 32, backgroundColor: Colors.green, color: Colors.black);
+            }
+            isRun;
+          });
+        },
+        child: Text(buttonText, style: buttonStyle,));
+  }
+
+  Future show() async {
+    while (isRun && stopwatch.getEnd() == null) {
+      await Future.delayed(const Duration(milliseconds: 1), () {});
+      setState(() {
+        dif = stopwatch.difference();
+      });
+    }
+  }
+}
